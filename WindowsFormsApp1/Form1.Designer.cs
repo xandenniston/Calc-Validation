14f7564bd21f8c944b9dfdcca5944fe8417905b9
Xan Denniston <xan@ksu.edu>
2019-10-08 19:56:05 -0500

    Infix working?

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,12 +29,78 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.uxInputBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.uxInfix = new System.Windows.Forms.Button();
+            this.uxPostFix = new System.Windows.Forms.Button();
+            this.uxPrefix = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxInputBox
+            // 
+            this.uxInputBox.Location = new System.Drawing.Point(12, 29);
+            this.uxInputBox.Name = "uxInputBox";
+            this.uxInputBox.Size = new System.Drawing.Size(666, 20);
+            this.uxInputBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // uxInfix
+            // 
+            this.uxInfix.Location = new System.Drawing.Point(696, 33);
+            this.uxInfix.Name = "uxInfix";
+            this.uxInfix.Size = new System.Drawing.Size(70, 27);
+            this.uxInfix.TabIndex = 2;
+            this.uxInfix.Text = "Infix";
+            this.uxInfix.UseVisualStyleBackColor = true;
+            this.uxInfix.Click += new System.EventHandler(this.uxInfix_Click);
+            // 
+            // uxPostFix
+            // 
+            this.uxPostFix.Location = new System.Drawing.Point(696, 66);
+            this.uxPostFix.Name = "uxPostFix";
+            this.uxPostFix.Size = new System.Drawing.Size(70, 27);
+            this.uxPostFix.TabIndex = 3;
+            this.uxPostFix.Text = "PostFix";
+            this.uxPostFix.UseVisualStyleBackColor = true;
+            this.uxPostFix.Click += new System.EventHandler(this.uxPostFix_Click);
+            // 
+            // uxPrefix
+            // 
+            this.uxPrefix.Location = new System.Drawing.Point(696, 99);
+            this.uxPrefix.Name = "uxPrefix";
+            this.uxPrefix.Size = new System.Drawing.Size(70, 27);
+            this.uxPrefix.TabIndex = 4;
+            this.uxPrefix.Text = "Prefix";
+            this.uxPrefix.UseVisualStyleBackColor = true;
+            this.uxPrefix.Click += new System.EventHandler(this.uxPrefix_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uxPrefix);
+            this.Controls.Add(this.uxPostFix);
+            this.Controls.Add(this.uxInfix);
+            this.Controls.Add(this.uxInputBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox uxInputBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button uxInfix;
+        private System.Windows.Forms.Button uxPostFix;
+        private System.Windows.Forms.Button uxPrefix;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,68 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void uxInfix_Click(object sender, EventArgs e)
+        {
+            if (InfixValidation())
+            {
+                MessageBox.Show("valid");
+            }
+            else
+            {
+                MessageBox.Show("invalid");
+
+            }
+        }
+
+        private void uxPostFix_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxPrefix_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool InfixValidation()
+        {
+            String input = uxInputBox.Text;
+            input = input.Replace(" ", string.Empty);//remove all spaces
+            Stack<char> parentheses = new Stack<char>();//hopefully reminiscent of parentheses lab
+            if(input.Length == 0) { return false; }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '(')
+                {
+                    parentheses.Push('(');
+                }
+                else if (input[i] == ')')
+                {
+                    if (i == 0){ return false; }// an expression cannot begin with a close parentheses
+                    if (input[i - 1] == '(') { return false; }// "()" is not valid
+                    if (parentheses.Count == 0) { return false; } // if the parentheses have run out
+                    else { parentheses.Pop(); }
+
+                }
+
+                if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')//in infix, all signs should have a number after it
+                {
+                    if (i == 0) { return false; }// an expression cannot begin with a sign in infix
+                    if((input[i-1] < '0' || input[i-1] > '9') && input[i-1] != ')') { return false; }
+
+                }
+            }
+            if (parentheses.Count != 0)//if the stack has not been emptied, there are extra parentheses
+            {
+                Console.WriteLine("too many close parentheses");
+                return false;
+            }
+            return true;
+        }
+        private bool PostfixValidation()
+        {
+
+        }
     }
 }
